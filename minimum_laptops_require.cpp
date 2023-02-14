cd93c6482fd804b839c2690fc9345dcd7992e2c5
Mahesh palaka <palakamahesh48325@gmail.com>
2023-02-14 21:30:36 +0530

    Create minimum_laptops_require.cpp

---
+++ minimum_laptops_require.cpp
@@ -0,0 +1,35 @@
+tatic bool comp(const pair<int,int>&a,const pair<int,int>&b)
+    {
+      if(a.first==b.first)
+        return a.second<b.second;
+      return a.fi
+        
+        //N = 3
+start[] = {1, 2, 3}
+end[] = {4, 4, 6}rst<b.first;
+    }
+    int minLaptops(int N, int start[], int end[]) {
+        // Code here
+        //int x=sizeof(start)/sizeof(start[0]);
+        int t=0;
+        vector<pair<int,int>>prefix;
+        for (int i=0;i<N;i++){
+            prefix.push_back({start[i],1});
+            prefix.push_back({end[i],-1});
+        }
+        
+        sort(prefix.begin(),prefix.end(),comp);
+        int cnt=0;
+        int maxi=0;
+        for (int i=0;i<2*N;i++){
+            maxi+=prefix[i].second;
+            cnt=max(cnt,maxi);
+        }
+        
+        return cnt;
+      
+//  N = 3
+// start[] = {1, 2, 3}
+// end[] = {4, 4, 6}
+//       Expected Time Complexity: O(N*logN)
+// Expected Auxiliary Space: O(N)
